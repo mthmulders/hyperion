1e7a94a0e9624b90f57f48ff0a9f76e61ef8c39b
Maarten Mulders <m.th.mulders@xs4all.nl>
2015-12-25 13:33:03 +0100

    Make the CollectingActor simpler and more robust

--- meter-agent/src/main/scala/hyperion/CollectingActor.scala
+++ meter-agent/src/main/scala/hyperion/CollectingActor.scala
@@ -1,12 +1,13 @@
 package hyperion
 
 import akka.actor.{ActorRef, ActorLogging, Actor, Props}
-import hyperion.CollectingActor.TelegramReceived
 
 import scala.collection.mutable
 import scala.collection.immutable
 
 object CollectingActor {
+  case object ProcessBuffer
+
   case class TelegramReceived(lines: immutable.Seq[String])
   def props(receiver: ActorRef): Props = {
     Props(new CollectingActor(receiver))
@@ -15,52 +16,45 @@ object CollectingActor {
 
 /** This actor collects incoming bytes and emits them in lines (seperated by CR LF) */
 class CollectingActor(receiver: ActorRef) extends Actor with ActorLogging {
+  import CollectingActor._
+
   private val CRLF = "\r\n"
 
-  override def receive = skippingFirstTelegram(new StringBuilder)
+  private val dataBuffer = new StringBuilder
+  private var lineBuffer = mutable.Buffer.empty[String]
 
-  /**
-    * In this (initial) state, we skip data until we found a new Telegram.
-    * @param dataBuffer Buffer of data that has already been received, but is not processed.
-    */
-  def skippingFirstTelegram(dataBuffer: StringBuilder): Receive = {
+  override def receive = {
     case MeterAgent.IncomingData(data) =>
       dataBuffer.append(data)
-      var linebreak = dataBuffer.indexOf(CRLF)
-      while (linebreak != -1) {
-        dataBuffer.replace(0, linebreak + CRLF.length, "")
-        linebreak = dataBuffer.indexOf(CRLF)
-      }
-      if (dataBuffer.startsWith("/")) {
-        context become bufferingTelegram(dataBuffer, mutable.Buffer.empty[String])
-      } else {
-        context become skippingFirstTelegram(dataBuffer)
-      }
+      extractLines()
+      extractTelegram()
   }
 
-  /**
-    * In this (normal) state, we buffer data until we found a new line.
-    * @param dataBuffer Buffer of data that has already been received, but is not processed.
-    * @param lineBuffer Buffer of lines that have already been received, but are not processed
-    */
-  def bufferingTelegram(dataBuffer: StringBuilder, lineBuffer: mutable.Buffer[String]): Receive = {
-    case MeterAgent.IncomingData(data) =>
-      dataBuffer.append(data)
-      var linebreak = dataBuffer.indexOf(CRLF)
-      while (linebreak != -1) {
-        val line = dataBuffer.substring(0, linebreak + CRLF.length)
-        lineBuffer += line.replace("\n", "").replace("\r", "")
-        dataBuffer.replace(0, linebreak + CRLF.length, "")
+  private def extractLines(): Unit = {
+    var linebreak = dataBuffer.indexOf(CRLF)
+    while (linebreak != -1) {
+      lineBuffer += dataBuffer.substring(0, linebreak)
+      dataBuffer.replace(0, linebreak + CRLF.length, "")
+      linebreak = dataBuffer.indexOf(CRLF)
+    }
+  }
 
-        if (lineBuffer.last.startsWith("!")) {
-          // The line buffer now contains a complete Telegram. Emit it to our receiver and clear the buffer.
-          receiver ! TelegramReceived(lineBuffer.toIndexedSeq)
-          lineBuffer.clear()
-          linebreak = -1
-        } else {
-          linebreak = dataBuffer.indexOf(CRLF)
-        }
-      }
-      context become bufferingTelegram(dataBuffer, lineBuffer)
+  private def extractTelegram(): Unit = {
+    if (lineBuffer.isEmpty) {
+      return
+    }
+
+    val firstLine = lineBuffer.find(s => s != null && s.length > 0 && s.charAt(0) == '/')
+    val lastLine = lineBuffer.find(s => s != null && s.length > 0 && s.charAt(0) == '!')
+
+
+    if (firstLine.isEmpty && lastLine.isDefined) {
+      lineBuffer --= lineBuffer.slice(0, lineBuffer.indexOf(lastLine.get) + 1)
+    } else if (lastLine.isDefined) {
+      val telegram = lineBuffer.slice(0, lineBuffer.indexOf(lastLine.get) + 1)
+      lineBuffer --= telegram
+
+      receiver ! TelegramReceived(telegram.toIndexedSeq)
+    }
   }
 }
