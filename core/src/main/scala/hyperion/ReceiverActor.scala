f9696a9d613cfa1ca7d0d68a4c947eaea8fc0a86
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-03-07 20:42:25 +0100

    ReceiverActor should forward messages it receives

--- core/src/main/scala/hyperion/ReceiverActor.scala
+++ core/src/main/scala/hyperion/ReceiverActor.scala
@@ -1,15 +1,26 @@
 package hyperion
 
-import akka.actor.{Actor, ActorLogging, Props}
+import scala.collection.mutable
+
+import akka.actor.{ActorRef, Actor, ActorLogging, Props}
+import hyperion.ReceiverActor.RegisterReceiver
 
 object ReceiverActor {
   def props(): Props = {
     Props(new ReceiverActor)
   }
+
+  case object RegisterReceiver
 }
 
 class ReceiverActor extends Actor with ActorLogging {
+  val receivers = mutable.Buffer.empty[ActorRef]
+
   override def receive = {
-    case msg => log.debug("Got message [{}]", msg)
+    case RegisterReceiver =>
+      receivers += sender()
+    case msg =>
+      log.debug("Got message [{}]", msg)
+      receivers.foreach(_ forward msg)
   }
 }
--- core/src/test/scala/hyperion/ReceiverActorSpec.scala
+++ core/src/test/scala/hyperion/ReceiverActorSpec.scala
@@ -1,6 +1,7 @@
 package hyperion
 
-import akka.testkit.EventFilter
+import akka.testkit.{EventFilter, TestProbe}
+import hyperion.ReceiverActor.RegisterReceiver
 
 class ReceiverActorSpec extends BaseAkkaSpec {
   "The Receiver Actor" should {
@@ -13,6 +14,20 @@ class ReceiverActorSpec extends BaseAkkaSpec {
       EventFilter.debug(message = s"Got message [$msg]", occurrences = 1) intercept {
         receiver ! msg
       }
+    }
+
+    "forward messages it receives to all recipients" in {
+      // Arrange
+      val receiver = system.actorOf(ReceiverActor.props(), "forward-messages")
+      val msg = "msg"
+      val probe = TestProbe()
+
+      // Act
+      probe.send(receiver, RegisterReceiver)
+      receiver ! msg
+
+      // Assert
+      probe.expectMsgAllOf(msg)
     }
   }
 }
