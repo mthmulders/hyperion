b49cc9160c2faf8581148c25342974836bdbda04
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-03-09 21:46:55 +0100

    Simple refusion of requests that are not allowed or supported

--- core/src/main/scala/hyperion/IncomingHttpActor.scala
+++ core/src/main/scala/hyperion/IncomingHttpActor.scala
@@ -1,15 +1,28 @@
 package hyperion
 
 import akka.actor.{ActorLogging, Actor, Props}
+import spray.can.Http
+import spray.http.{HttpRequest, HttpResponse, StatusCodes, Uri}
 
+/** Companion object voor the [[IncomingHttpActor]] class */
 object IncomingHttpActor {
   def props(): Props = {
     Props(new IncomingHttpActor())
   }
 }
 
+/**
+  * Acts as a router for incoming HTTP calls.
+  */
 class IncomingHttpActor extends Actor with ActorLogging {
   override def receive: Receive = {
-    case msg: Any => log.debug("Message {} not handled", msg)
+    // When a new TCP connection comes in, we register ourselves as the Actor who will handle it.
+    case _: Http.Connected =>
+      sender ! Http.Register(self)
+
+    // All other requests are logged but ignored
+    case HttpRequest(method, Uri.Path(path), _, _, _) =>
+      log.info("{} request to {} not supported", method, path)
+      sender() ! HttpResponse(status = StatusCodes.NotFound, entity = "Not found")
   }
 }
---
+++ core/src/test/scala/hyperion/IncomingHttpActorSpec.scala
@@ -0,0 +1,43 @@
+package hyperion
+
+import akka.actor.ActorDSL.actor
+import akka.testkit.TestProbe
+import org.scalatest.OptionValues
+import spray.http.{HttpEntity, HttpRequest, HttpResponse, StatusCodes, Uri}
+import spray.http.ContentTypes.`application/json`
+import spray.http.HttpMethods.{GET, POST, PUT}
+import spray.http.HttpProtocols.`HTTP/1.1`
+
+class IncomingHttpActorSpec extends BaseAkkaSpec with OptionValues {
+  "Receiving an incoming HTTP request" should {
+    "reject requests to the wrong path" in {
+      // Arrange
+      val client = TestProbe()
+      val request = new HttpRequest(GET, Uri("/wrong"), Nil, HttpEntity.Empty, `HTTP/1.1`)
+
+      // Act
+      val sut = actor("wrong-path")(new IncomingHttpActor())
+      client.send(sut, request)
+
+      // Assert
+      val response = client.expectMsgAllClassOf(classOf[HttpResponse]).headOption
+      response.value.status should be(StatusCodes.NotFound)
+      response.value.entity.asString should include("Not found")
+    }
+
+    "reject requests with the wrong HTTP method" in {
+      // Arrange
+      val client = TestProbe()
+      val request = new HttpRequest(PUT, Uri("/"), Nil, HttpEntity.Empty, `HTTP/1.1`)
+
+      // Act
+      val sut = actor("wrong-method")(new IncomingHttpActor())
+      client.send(sut, request)
+
+      // Assert
+      val response = client.expectMsgAllClassOf(classOf[HttpResponse]).headOption
+      response.value.status should be(StatusCodes.NotFound)
+      response.value.entity.asString should include("Not found")
+    }
+  }
+}
