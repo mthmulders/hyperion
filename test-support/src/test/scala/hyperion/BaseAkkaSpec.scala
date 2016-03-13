04d49171f2fa0d75e2115734d40f49c09716372a
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-03-13 11:47:46 +0100

    Simplify code for verifying actors exist

--- test-support/src/test/scala/hyperion/BaseAkkaSpec.scala
+++ test-support/src/test/scala/hyperion/BaseAkkaSpec.scala
@@ -4,26 +4,26 @@ import akka.actor.{ActorIdentity, ActorRef, ActorSystem, Identify}
 import akka.testkit.{EventFilter, TestEvent, TestProbe}
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.{BeforeAndAfterAll, Matchers}
+import org.slf4j.LoggerFactory
 
 import scala.collection.{immutable, mutable}
 import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, DurationInt, FiniteDuration}
 
 abstract class BaseAkkaSpec extends BaseSpec with Matchers with BeforeAndAfterAll with TypeCheckedTripleEquals {
+  protected[this] val log = LoggerFactory.getLogger(getClass)
+
   implicit class TestProbeOps(probe: TestProbe) {
     private val maxWaitForActor = 100 milliseconds
 
     def expectActor(path: String, max: FiniteDuration = maxWaitForActor): immutable.Seq[ActorRef] = {
-      probe.within(max) {
-        val actors = mutable.ArrayBuffer.empty[ActorRef]
-        probe.awaitAssert {
-          (probe.system actorSelection path).tell(Identify(path), probe.ref)
-          probe.expectMsgPF(100 milliseconds) {
-            case ActorIdentity(`path`, Some(ref)) => actors += ref
-          }
-        }
-        immutable.Seq(actors:_*)
+      val actors = mutable.ArrayBuffer.empty[ActorRef]
+      (probe.system actorSelection path).tell(Identify(path), probe.ref)
+      probe.expectMsgPF(max) {
+        case ActorIdentity(`path`, Some(ref)) => actors += ref
+        case ActorIdentity(`path`, None) => log.warn("Expected Some(ActorRef) for path {} but got None", path)
       }
+      immutable.Seq(actors:_*)
     }
   }
 
