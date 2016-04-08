ed82416ca9bd94c6133c55ad96bfce8303602d07
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-04-08 10:27:24 +0200

    Adds a size-limited buffer for storing arbitrary data

---
+++ core/src/main/scala/hyperion/RingBuffer.scala
@@ -0,0 +1,103 @@
+package hyperion
+
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
+import scala.annotation.migration
+import scala.collection.generic.CanBuildFrom
+import scala.collection.immutable.IndexedSeq
+import scala.collection._
+import scala.collection.parallel.{ParSeq, Combiner}
+import scala.reflect.ClassTag
+
+/**
+  * A collection that is limited in size and will purge the oldest items when the limit is exceeded.
+  * This allows for efficient memory allocation and drops the need to re-allocate arrays.
+ *
+  * @param limit Maximum number of elements in this collection.
+  */
+class RingBuffer[T](limit: Int)(implicit m: ClassTag[T]) extends mutable.AbstractBuffer[T] with GenTraversable[T] {
+  private[this] val items = new Array[T](limit)
+  private[this] var cursor = 0
+  private[this] val monitor = new ReentrantReadWriteLock()
+
+  private[this] def positionInArray(desiredPosition: Int): Int = {
+    (cursor + desiredPosition) % limit
+  }
+
+  override def apply(n: Int): T = {
+    monitor.readLock().lock()
+    try {
+      items(positionInArray(n))
+    } finally {
+      monitor.readLock().unlock()
+    }
+  }
+
+  override def update(n: Int, newelem: T): Unit = {
+    monitor.writeLock().lock()
+    try {
+      items.update(positionInArray(n), newelem)
+    } finally {
+      monitor.writeLock().unlock()
+    }
+  }
+
+  override def clear(): Unit = {}
+
+  override def length: Int = items.count(_ != null)
+
+  /**
+    * Removes the element at a given index from this buffer.
+    * Maintains the contract, but does not actually remove the element.
+    *
+    * @param n the index which refers to the element to delete.
+    * @return the previous element at index `n`.
+    */
+  override def remove(n: Int): T = apply(n)
+
+  /** @inheritdoc */
+  override def +=:(elem: T): RingBuffer.this.type = ???
+
+  /** @inheritdoc */
+  override def +=(elem: T): RingBuffer.this.type = {
+    monitor.writeLock().lock()
+    try {
+      items.update(cursor, elem)
+      cursor += 1
+      if (cursor >= limit) cursor = 0
+      this
+    } finally {
+      monitor.writeLock().unlock()
+    }
+  }
+
+  override def insertAll(n: Int, elems: scala.Traversable[T]): Unit = ???
+
+  /** @inheritdoc */
+  override def iterator: scala.Iterator[T] = {
+    new RingBufferIterator[T](this, cursor, monitor)
+  }
+}
+
+object RingBuffer {
+  def apply[T: ClassTag](size: Int) = {
+    new RingBuffer[T](size)
+  }
+}
+
+class RingBufferIterator[T](src: RingBuffer[T], startPos: Int, monitor: ReentrantReadWriteLock) extends Iterator[T] {
+  private[this] var cursor = 0
+
+  override def hasNext: Boolean = cursor < src.length
+
+  override def next(): T = {
+    monitor.readLock().lock()
+    try {
+      val r = src(cursor)
+      cursor += 1
+      r
+    } finally {
+      monitor.readLock().unlock()
+    }
+  }
+}
---
+++ core/src/test/scala/hyperion/RingBufferSpec.scala
@@ -0,0 +1,73 @@
+package hyperion
+
+class RingBufferSpec extends BaseSpec {
+  "A RingBuffer" should {
+    "not contain more elements than its limit allows" in {
+      // Arrange
+      val buffer = RingBuffer[Object](3)
+
+      // Act
+      buffer += new Object
+      buffer += new Object
+      buffer += new Object
+      buffer += new Object
+
+      // Assert
+      buffer.length should be (3)
+    }
+
+    "overwrite the oldest item when adding an element that would exceed the limit" in {
+      // Arrange
+      val buffer = RingBuffer[Int](3)
+
+      // Act
+      buffer += 1
+      buffer += 2
+      buffer += 3
+      buffer += 4
+
+      // Assert
+      buffer should contain inOrderOnly (2, 3, 4)
+    }
+
+    "correctly report the number of items it contains when it is not fully occupied" in {
+      // Arrange
+      val buffer = RingBuffer[Object](3)
+
+      // Act
+      buffer += new Object
+      buffer += new Object
+
+      // Assert
+      buffer.length should be (2)
+    }
+
+    "correctly loop over the buffer if its limit was exceeded" in {
+      // Arrange
+      val buffer = RingBuffer[String](3)
+
+      // Act
+      buffer += "1"
+      buffer += "2"
+      buffer += "3"
+      buffer += "4"
+      buffer += "5"
+
+      // Assert
+      buffer should contain inOrderOnly ("3", "4", "5")
+    }
+
+    "replace items in the buffer without touching other elements" in {
+      // Arrange
+      val buffer = RingBuffer[String](2)
+      buffer += "1"
+      buffer += "2"
+
+      // Act
+      buffer.update(0, "foo")
+
+      // Assert
+      buffer should contain inOrderOnly ("foo", "2")
+    }
+  }
+}
