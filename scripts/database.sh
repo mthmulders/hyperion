f072baa9b38145c7eddbc8ee315899b1c1b7d4b9
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-07-20 09:55:12 +0200

    Migrate from MySQL to PostgreSQL

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker pull mtirsel/mysql-5.1
+docker pull postgres:9.3
 
-docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=welcome123 -e MYSQL_DATABASE=test -d mtirsel/mysql-5.1
+docker run --name hyperion-db -p 5432:5432 \
+    -e POSTGRES_DB=hyperion \
+    -e POSTGRES_USER=hyperion \
+    -e POSTGRES_PASSWORD=welcome123 \
+    -d postgres:9.3
