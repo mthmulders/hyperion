28ba3b6543b399720559ec5d95a16ee4969ef676
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-07-28 10:45:25 +0200

    Actually test something

--- app/src/test/scala/hyperion/ws/ActualReadingsServiceSpec.scala
+++ app/src/test/scala/hyperion/ws/ActualReadingsServiceSpec.scala
@@ -1,34 +1,23 @@
 package hyperion.ws
 
-import akka.actor.ActorRef
-import akka.testkit.TestActor.AutoPilot
-import akka.testkit.{TestActor, TestProbe}
+import akka.testkit.TestProbe
 import hyperion.rest.HyperionJsonProtocol
 import hyperion.BaseSpec
-import spray.http.HttpRequest
+import hyperion.MessageDistributor.RegisterReceiver
 import spray.testkit.ScalatestRouteTest
 
 class ActualReadingsServiceSpec extends BaseSpec with ScalatestRouteTest with HyperionJsonProtocol {
   "The ActualReadings WebSocket API" should {
-    "delegate to upgrade the connection to a WebSocket" in {
+    "should link the client to the message distributor" in {
       // Arrange
-      val connectionUpgrade = new Object // real type doesn't matter here
       val messageDistributor = TestProbe()
-      messageDistributor.setAutoPilot(new AutoPilot {
-        override def run(sender: ActorRef, msg: Any): AutoPilot = {
-          msg match {
-            case HttpRequest => sender ! connectionUpgrade
-            case m => logger.warn(s"Unexecpted message $m")
-          }
-          TestActor.KeepRunning
-        }
-      })
       val route = new ActualReadingsService(messageDistributor.ref, system).route
 
         // Act
-      Get("/actual") ~> route ~> check {
-        // Assert
-      }
+      Get("/actual") ~> route
+
+      // Assert
+      messageDistributor.expectMsg(RegisterReceiver)
     }
   }
 }
